4499f63abcc08bb77025c8ed91f2c7d5a4fbf672
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2023-07-03 15:13:18 +0900

    make site
    Make site but no translate code

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
 const http = require('http');
+const fs = require('fs');
+const { createWriteStream } = require('fs');
+const MidiWriter = require('midi-writer-js');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 4000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
+  if (req.method === 'GET' && req.url === '/') {
+    // Send HTML file for the homepage
+    fs.readFile('index.html', 'utf8', (err, content) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Server Error');
+        return;
+      }
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(content);
+    });
+  } else if (req.method === 'POST' && req.url === '/upload') {
+    // Handle file upload and conversion
+    let data = '';
+    
+    req.on('data', chunk => {
+      data += chunk;
+    });
+    
+    req.on('end', () => {
+      // Convert data to MIDI file using MidiWriter
+      const file = new MidiWriter.Writer();
+      // Add your MIDI file creation logic here
+      
+      // Save MIDI file
+      const fileName = 'output.mid';
+      const fileStream = createWriteStream(fileName);
+      fileStream.write(new Uint8Array(file.buildFile()));
+      
+      fileStream.on('finish', () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ success: true }));
+      });
+      
+      fileStream.on('error', err => {
+        console.error('Error:', err);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ error: 'Internal Server Error' }));
+      });
+      
+      fileStream.end();
+    });
+  } else {
+    res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, bye\n');
+    res.end('Not Found');
+  }
 });
 
 server.listen(port, hostname, () => {
-    console.log('Server running at http://${hostname}: ${prot}/');
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
